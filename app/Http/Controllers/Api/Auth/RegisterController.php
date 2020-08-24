8e972e5f7c0db7bcbde1953b999218c05bfe444c
IDStack <idstack@IDStacks-MacBook-Pro.local>
2020-08-25 01:15:52 +0700

    install sanctum and auth api

---
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class RegisterController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
+            'device_name' => ['required']
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        if (! Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'The provided credentials are incorrect.',
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $accessToken = Auth::user()->createToken($request->device_name)->plainTextToken;
+
+        return response()->json([
+            'message' => 'success',
+            'data' => $user,
+            'meta' => [
+                'token' => $accessToken
+            ]
+        ], Response::HTTP_CREATED);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/auth/register', 'Api\Auth\RegisterController');
+Route::post('/auth/login', 'Api\Auth\LoginController@login');
+Route::post('/auth/logout', 'Api\Auth\LoginController@logout')
+    ->middleware('auth:sanctum');
